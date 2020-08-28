1c4ff6de353a520b992fa25ea0b566b85aaef8d6
acroipeol <copomister@gmail.com>
2020-08-28 14:54:47 +0800

    Updates

---
+++ precache-manifest.edc09598091ff47803a0913957ca4278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c4ee3221ae680b46d99",
+    "url": "./static/css/main.00a7837e.chunk.css"
+  },
+  {
+    "revision": "1c4ee3221ae680b46d99",
+    "url": "./static/js/main.1c4ee322.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "0b9347c28d33cf250f48",
+    "url": "./static/css/2.39665907.chunk.css"
+  },
+  {
+    "revision": "0b9347c28d33cf250f48",
+    "url": "./static/js/2.0b9347c2.chunk.js"
+  },
+  {
+    "revision": "12d518def262567b8567b2b64e76730e",
+    "url": "./index.html"
+  }
+];
